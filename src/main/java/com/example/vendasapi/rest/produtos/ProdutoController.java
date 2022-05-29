d533b4ecb266f080626ded13c717cc11c332dc5b
Arnaldo Pagani Junior <arnaldopaganijr@gmail.com>
2022-05-28 21:40:43 -0300

    criado metodo post

--- src/main/java/com/example/vendasapi/rest/produtos/ProdutoController.java
+++ src/main/java/com/example/vendasapi/rest/produtos/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.Optional;
@@ -17,15 +18,26 @@ public class ProdutoController {
     private ProdutoRepository repository;
 
     @GetMapping("/home")
-    ResponseEntity<String> home(){
+    ResponseEntity<String> home() {
         return ResponseEntity.ok("oi");
     }
 
     @PostMapping("/produtos")
     @CrossOrigin("*")
     @ResponseStatus(HttpStatus.CREATED)
-    public  Produto salvar(@Valid @RequestBody Produto produto){
-        return repository.save(produto);
+    ResponseEntity<Produto> save(@Valid @RequestBody Produto produto) {
+        return ResponseEntity.ok(repository.save(produto));
     }
 
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> update(@PathVariable long id, @Valid @RequestBody Produto produtoAtualizado){
+        Optional<Produto> produtoExistente = repository.findById(id);
+        if(produtoExistente.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        produtoAtualizado.setId(id);
+        repository.save(produtoAtualizado);
+        return ResponseEntity.ok().build();
+    }
 }
