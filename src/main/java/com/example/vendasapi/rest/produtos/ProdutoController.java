40e486ac3f3fe07f0e62180c5cf32980efe4c70b
Arnaldo Pagani Junior <arnaldopaganijr@gmail.com>
2022-05-30 21:53:32 -0300

    criado metodo post

--- src/main/java/com/example/vendasapi/rest/produtos/ProdutoController.java
+++ src/main/java/com/example/vendasapi/rest/produtos/ProdutoController.java
@@ -9,27 +9,23 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/produtos")
 public class ProdutoController {
     @Autowired
     private ProdutoRepository repository;
 
-    @GetMapping("/home")
-    ResponseEntity<String> home() {
-        return ResponseEntity.ok("oi");
-    }
-
-    @PostMapping("/produtos")
+    @PostMapping
     @CrossOrigin("*")
     @ResponseStatus(HttpStatus.CREATED)
     ResponseEntity<Produto> save(@Valid @RequestBody Produto produto) {
         return ResponseEntity.ok(repository.save(produto));
     }
 
-    @PutMapping("/produtos/{id}")
+    @PutMapping("/{id}")
     @CrossOrigin("*")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> update(@PathVariable long id, @Valid @RequestBody Produto produtoAtualizado){
@@ -40,5 +36,9 @@ public class ProdutoController {
         produtoAtualizado.setId(id);
         repository.save(produtoAtualizado);
         return ResponseEntity.ok().build();
+    }
+    @GetMapping
+    public List<Produto> findAll(){
+        return repository.findAll();
     }
 }
