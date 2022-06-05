b29667aca3cb742395f0d2dcc67f52add0036fce
Arnaldo Pagani Junior <arnaldopaganijr@gmail.com>
2022-06-05 19:56:38 -0300

    criado metodo post

--- src/main/java/com/example/vendasapi/rest/produtos/ProdutoController.java
+++ src/main/java/com/example/vendasapi/rest/produtos/ProdutoController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -14,19 +13,19 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/produtos")
+@CrossOrigin("*")
 public class ProdutoController {
     @Autowired
     private ProdutoRepository repository;
 
     @PostMapping
-    @CrossOrigin("*")
+
     @ResponseStatus(HttpStatus.CREATED)
     ResponseEntity<Produto> save(@Valid @RequestBody Produto produto) {
         return ResponseEntity.ok(repository.save(produto));
     }
 
-    @PutMapping("/{id}")
-    @CrossOrigin("*")
+    @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> update(@PathVariable long id, @Valid @RequestBody Produto produtoAtualizado){
         Optional<Produto> produtoExistente = repository.findById(id);
@@ -38,6 +37,24 @@ public class ProdutoController {
         return ResponseEntity.ok().build();
     }
     @GetMapping
-    @CrossOrigin("*")
     public List<Produto> findAll() {return repository.findAll();}
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<Produto>> getById (@PathVariable Long id){
+        Optional<Produto> produto = repository.findById(id);
+        if(produto.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(produto);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar (@PathVariable long id){
+        Optional<Produto> produto = repository.findById(id);
+        if(produto.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        repository.delete(produto.get());
+        return ResponseEntity.noContent().build();
+    }
+
 }
